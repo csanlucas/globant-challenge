ec4c38b72c20df1100c759e31eddbdc2ecfba707
Cesar San Lucas <csanlucas@live.com>
2023-01-31 20:36:34 -0500

    Use secrets for DB creds and settings

--- weatherapi/settings.py
+++ weatherapi/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+from .secrets import Config
+from .environments import ALLOWED_ENVS, ENV_LOCAL, ENV_PROD
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,6 +28,13 @@ SECRET_KEY = 'django-insecure-_8z8f84_+fqucb%)773i(284vebm#wi&omo1#xrkkp509q+t6f
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+# Current ENVIRONMENT config
+GLOBANTAPI_ENV = os.environ.get('GLOBANTAPI_ENV', ENV_LOCAL)
+if GLOBANTAPI_ENV == ENV_PROD:
+    DEBUG = False
+if GLOBANTAPI_ENV not in ALLOWED_ENVS:
+    raise Exception('GLOBANTAPI_ENV must be on of ({})'.format(ALLOWED_ENVS))
 
 ALLOWED_HOSTS = []
 
@@ -75,12 +86,37 @@ WSGI_APPLICATION = 'weatherapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+Config.set_database_credentials(env=GLOBANTAPI_ENV)
+
+if not Config.DATABASE_CRED:
+    raise Exception("Can not setup database configuration for {} not defined".format(GLOBANTAPI_ENV))
+
+if GLOBANTAPI_ENV == ENV_PROD:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': Config.DATABASE_CRED['NAME'],
+            'USER': Config.DATABASE_CRED['USER'],
+            'PASSWORD': Config.DATABASE_CRED['PASSWORD'],
+            'HOST': Config.DATABASE_CRED['HOST'],
+            'PORT': Config.DATABASE_CRED['PORT'],
+            'ATOMIC_REQUESTS': True
+        }
     }
-}
+elif GLOBANTAPI_ENV == ENV_LOCAL:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': Config.DATABASE_CRED['NAME'],
+            'USER': Config.DATABASE_CRED['USER'],
+            'PASSWORD': Config.DATABASE_CRED['PASSWORD'],
+            'HOST': Config.DATABASE_CRED['HOST'],
+            'PORT': Config.DATABASE_CRED['PORT'],
+            'ATOMIC_REQUESTS': True
+        }
+    }
+else:
+    raise Exception("Database configuration for {} not defined".format(HOUMAPI_ENV))
 
 
 # Password validation
